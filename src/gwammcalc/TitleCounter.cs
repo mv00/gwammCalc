35f9689b560a7aedfbf868fb02f79aae4632dbca
Matte <matthias>
2019-03-20 18:45:18 +0100

    Add initial project

---
+++ src/gwammcalc/TitleCounter.cs
@@ -0,0 +1,15 @@
+﻿namespace GwammCalc
+{
+    public static class TitleCounter
+    {
+        private static int _titleCount;
+
+        public static void AddTitle()
+        {
+            var oldtitleCount = _titleCount;
+            _titleCount++;
+        }
+        public static void RemoveTitle() => _titleCount--;
+        public static int CurrentTitleCount => _titleCount;
+    }
+}
