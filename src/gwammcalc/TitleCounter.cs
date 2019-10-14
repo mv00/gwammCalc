7db6e69fa8123dc30a10ea470e704c9684ad7e5e
mv00 <>
2019-10-14 21:53:41 +0200

    Clean code

--- src/gwammcalc/TitleCounter.cs
+++ src/gwammcalc/TitleCounter.cs
@@ -4,12 +4,9 @@
     {
         private static int _titleCount;
 
-        public static void AddTitle()
-        {
-            var oldtitleCount = _titleCount;
-            _titleCount++;
-        }
-        public static void RemoveTitle() => _titleCount--;
         public static int CurrentTitleCount => _titleCount;
+
+        public static void AddTitle() => _titleCount++;
+        public static void RemoveTitle() => _titleCount--;
     }
 }
